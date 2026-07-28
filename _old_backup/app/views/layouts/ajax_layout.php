d492b84d78b49a6dc2310179eb14c2c7674bccd6
vn002-tech <vandv190220@gmail.com>
2026-07-28 16:47:13 +0700

    Remove _old_backup from tracking index

--- _old_backup/app/views/layouts/ajax_layout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// Set url variable from router class
-	$page_name = Router::$page_name;
-	$page_action = Router::$page_action;
-	$page_id = Router::$page_id;
-	$this->render_body();
-	//rebind jquery plugins
-	Html :: page_js("plugins-init.js");
-?>
